5efcab49033af981a83906ce74ea8c472c3959d1
= <=>
2021-09-05 11:11:02 +0300

    second part

--- lessons/2/js/script.js
+++ lessons/2/js/script.js
@@ -12,3 +12,30 @@ let userSurname;
 
 userSurname = "Chabaniuk";
 console.log(userSurname);
+let newUser;
+
+newUser = "New user";
+console.log(newUser);
+
+
+let shoppingList;
+
+shoppingList = "Shopping list"
+console.log(shoppingList);
+
+let statusUser;
+
+statusUser = "Online status";
+console.log(statusUser);
+
+
+let salary;
+
+salary = "Employee`s salary";
+console.log(salary);
+
+
+let coordinatesMouse;
+
+coordinatesMouse = "Cursor (mouse) coordinates.";
+console.log(coordinatesMouse);
