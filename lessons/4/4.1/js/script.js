825d719245f2a23953fb56be3a7e1be5dedfdbef
= <=>
2021-09-19 13:29:40 +0300

    4.1.1

--- lessons/4/4.1/js/script.js
+++ lessons/4/4.1/js/script.js
@@ -8,11 +8,23 @@ function getSum() {
    console.log('Сума', sum);
 
    alert("Сума: " + sum);
+   return num1 + num2;
 }
 
 getSum()
 
 
+const num1 = Number(prompt("Перше число:"));
+console.log('Перше число:', num1);
+const num2 = Number(prompt("Друге число:"));
+console.log('Друге число:', num2);
+const sum = (a, b) => a + b;
+const res1 = sum(num1, num2)
+alert("Сума: " + res1);
+console.log('Сума', res1);
+
+
+/*
 function getComparison() {
    const num1 = Number(prompt("Перше число:"));
    console.log('Перше число:', num1);
@@ -28,3 +40,4 @@ function getComparison() {
 }
 
 getComparison()
+*/
---
+++ lessons/4/4.3/js/script.js
@@ -0,0 +1,25 @@
+/*function userData() {
+   const nameUser = prompt("Як до Вас звернутися?");
+   console.log('Користувача звати:', nameUser);
+   const userOld = Number(prompt("Який Ваш вік?"));
+   console.log('Вік користувача:', userOld);
+
+   alert("Раді Вас бачити " + nameUser);
+}
+
+userData()
+
+function userGreeting() {
+   const nameUser = prompt("Як до Вас звернутися?");
+   console.log('Користувача звати:', nameUser);
+   const userOld = Number(prompt("Який Ваш вік?"));
+   console.log('Вік користувача:', userOld);
+   if (userOld >= 30) {
+      alert("Вітаю " + nameUser);
+   } else {
+      alert("Привіт " + nameUser);
+   }
+}
+
+userGreeting()
+*/
