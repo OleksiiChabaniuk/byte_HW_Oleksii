04a2d4a71ed7f697b4e75b317d49ae9bed3ad452
= <=>
2021-11-08 16:48:35 +0200

    6.1

--- lessons/6/6.1/js/script.js
+++ lessons/6/6.1/js/script.js
@@ -1,38 +1,59 @@
-let userName;
-let userSurname;
-let userPassword;
+const isValidPassword = (password) =>
+   password.toLowerCase() !== password &&
+   password.toUpperCase() !== password.toLowerCase() &&
+   password.length >= 6;
 
+const formatUserData = (data) =>
+   data[0].toUpperCase() + data.slice(1).toLowerCase();
 
+const getUserData = () => {
+   let userName;
+   let userSurname;
+   let userPassword;
 
-function registrationuser() {
-   userName = prompt('Введіть Ім\`я:');
-   console.log(`userName ${userName}`);
-   if (!userName) {
-      alert('No date')
-      return;
-   }
+   do {
+      userName = prompt("Name?");
+      console.log(`userName ${userName}`);
+      if (!userName) {
+         alert('No date')
+         continue;
+         }
 
+      if (userName.length < 4) //длина меньше 4
+      {
+         alert('Занадто коротке ім\'я');
+         continue;
+      }
+      userSurname = prompt("Surname?");
+      console.log(`userSurname ${userSurname}`);
+      if (!userSurname) {
+         alert('No date')
+         continue;
+      }
 
+      if (userSurname.length < 4) { //длина меньше 4
+         alert('Занадто коротке прізвище');
+         continue;
+      }
+      userPassword = prompt("Password?");
+      console.log(`userPassword ${userPassword}`);
+      if (!userPassword) {
+         alert('No date')
+         continue;
+      }
+      if (!userPassword) {
+         alert('No date')
+         continue;
+      }
+      break;
+      }
+      while (!userName || !userSurname || !isValidPassword(userPassword));
 
+   const formattedName = formatUserData(userName)
+   const formattedSurname = formatUserData(userSurname)
+   alert(`Hello ${formattedName} ${formattedSurname}`);
+   console.log(`Hello ${formattedName} ${formattedSurname}`);
 
+};
 
-   userSurname = prompt('Введіть прізвище:');
-   console.log(`userSurname ${userSurname}`);
-   if (!userSurname) {
-      alert('No date')
-   }
-
-
-   userPassword = prompt('Введіть пароль:');
-   console.log(`userPassword ${userPassword}`);
-   if (!userPassword) {
-      alert('No date')
-   }
-
-}
-
-
-
-registrationuser()
-
-let fullName = alert(`${userName} ${userSurname}`);
+getUserData()
