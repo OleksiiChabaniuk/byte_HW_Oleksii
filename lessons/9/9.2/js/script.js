38fa7660cfc1103189072486d6fe9184abacc559
= <=>
2021-12-05 13:52:57 +0200

    11

--- lessons/9/9.2/js/script.js
+++ lessons/9/9.2/js/script.js
@@ -1,31 +1,54 @@
-const ownForEarch = (arr, callback) => {
-   for (let i = 0; i < arr.length; i++) {
-      callback(arr[i], i, arr);
-   }
-};
+const data = [{
+   name: "John",
+   age: 24,
+   position: "senior",
+   isActive: false,
+}, {
+   name: "Peter",
+   age: 33,
+   position: "middle",
+   isActive: false,
+}, {
+   name: "Sam",
+   age: 29,
+   position: "junior",
+   isActive: true,
+}, {
+   name: "Mary",
+   age: 24,
+   position: "middle",
+   isActive: false,
+}, {
+   name: "Steve",
+   age: 23,
+   position: "middle",
+   isActive: true,
+}, {
+   name: "Kate",
+   age: 31,
+   position: "middle",
+   isActive: false,
+}, {
+   name: "Sally",
+   age: 19,
+   position: "junior",
+   isActive: false,
+}, {
+   name: "Jack",
+   age: 19,
+   position: "middle",
+   isActive: true,
+}, ];
 
-const ownMap = (arr, callback) => {
-   let resultArr = [];
-
-   for (let i = 0; i < arr.length; i++) {
-      const operationResult = callback(arr[i], i, arr);
-
-      resultArr.push(operationResult);
-   }
-
-   return resultArr;
-};
-
-const ownFilter = (arr, callback) => {
-   let resultArr = [];
-
-   for (let i = 0; i < arr.length; i++) {
-      const operationResult = callback(arr[i], i, arr);
-
-      if (operationResult) {
-         resultArr.push(arr[i]);
-      }
-   }
-
-   return resultArr;
+function filterData(data, filterBy) {
+   return data.filter((item) => {
+      return Object.entries(filterBy).every(([key, value]) => {
+         return item[key] === value;
+      });
+   });
 }
+
+console.log(filterData(data, {
+   age: 19,
+   position: "junior"
+}));
